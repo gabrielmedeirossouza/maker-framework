0e13a434d87e39965c16882c507327ac572b95ff
Gabriel Medeiros Souza <gmsgabrielmedeiros@gmail.com>
2022-12-18 20:26:43 -0300

    test: unit test for random-uuid helper

---
+++ src/helpers/random-uuid/random-uuid.spec.ts
@@ -0,0 +1,35 @@
+import { describe, it, expect } from 'vitest';
+import { randomUUID } from '.';
+
+describe('randomUUID', () => {
+    it('should return a string', () => {
+        const uuid = randomUUID();
+
+        expect(typeof uuid).toBe('string');
+    });
+
+    it('should return a string of length 32', () => {
+        const uuid = randomUUID();
+
+        expect(uuid.length).toBe(32);
+    });
+
+    it('should return a string of lowercase characters', () => {
+        const uuid = randomUUID();
+
+        expect(uuid).toBe(uuid.toLowerCase());
+    });
+
+    it('should return a string without special characters', () => {
+        const uuid = randomUUID();
+
+        expect(uuid).toMatch(/^[\da-z]+$/);
+    });
+
+    it('should return different strings', () => {
+        const uuid1 = randomUUID();
+        const uuid2 = randomUUID();
+
+        expect(uuid1).not.toBe(uuid2);
+    });
+});
