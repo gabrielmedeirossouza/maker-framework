c6c7cb4955b1106d207587130e2e7a5d6ed00109
Gabriel Medeiros Souza <gmsgabrielmedeiros@gmail.com>
2022-12-18 20:25:04 -0300

    chore: add vitest and jsdom libs from unit tests

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [alias()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }]
   }
