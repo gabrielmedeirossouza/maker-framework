ec580a8309f46db51cb664d9b17d8ee183c72d11
Gabriel Medeiros Souza <gmsgabrielmedeiros@gmail.com>
2022-12-18 21:24:24 -0300

    style: add app/reactivity to new folder structure

--- src/app/reactivity/index.ts
+++ src/app/reactivity/index.ts
@@ -1,16 +1,1 @@
-import { Maker } from '@/app';
-import { Reactive } from './reactive';
-
-export class Reactivity {
-    private _maker: Maker;
-
-    constructor(maker: Maker) {
-        this._maker = maker;
-    }
-
-    public Reactive<T extends object>(params: T): T {
-        const reactive = new Reactive(this._maker,).Create(params);
-
-        return reactive;
-    }
-}
+export * from './reactivity';
